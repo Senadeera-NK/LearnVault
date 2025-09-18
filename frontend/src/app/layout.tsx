cc420b3c8c5409dd57ade725d29cda86c1bd4bc6
S.A. Nanduni Kaveesha Senadeera <66235626+Senadeera-NK@users.noreply.github.com>
2025-09-18 11:14:39 +0000

    changes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,28 @@
 // src/app/layout.tsx
 import { Providers } from "./providers";
 import Sidebar from "@/components/Sidebar";
+import Googlesignin from "./googlesignin/page";
+import { useSession } from "next-auth/react"
+import Google from "next-auth/providers/google";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const {data: session} = useSession();
   return (
     <html lang="en">
       <body>
         <Providers>
           <Sidebar />
-          <main style={{ padding: "1rem" }}>{children}</main>
+          <main style={{ padding: "1rem" }}>
+            {!session && <Googlesignin />}
+            {session &&(
+              <div>
+                <p>Logged in as: {session.user?.name}</p>
+                <p>Email: {session.user?.email}</p>
+                <Googlesignin />
+              </div>
+            )}
+            {children}
+            </main>
         </Providers>
       </body>
     </html>
