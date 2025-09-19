00f6335003175543a2fbb5dbb07a96a45b3c3623
S.A. Nanduni Kaveesha Senadeera <66235626+Senadeera-NK@users.noreply.github.com>
2025-09-19 08:05:25 +0000

    creatin the env of backend

---
+++ backend/services/supabase_config.py
@@ -0,0 +1,9 @@
+SUPABASE_URL = "https://idarabphqsrgaazkavon.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkYXJhYnBocXNyZ2Fhemthdm9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NTk1MjUsImV4cCI6MjA2NDAzNTUyNX0.5nF0hIxOTqojoIlU5UKEUemCR3EcgHu4CUbXD64yBKo"  # better to load via .env
+
+HEADERS = {
+    "apikey":SUPABASE_KEY,
+    "Authorization":f"Bearer {SUPABASE_KEY}",
+    "Content-Type":"application/json",
+    "Accept":"application/json"
+}
