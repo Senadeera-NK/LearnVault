aefee58f1a2c0593e0c1f09315970f66f407c9ac
S.A. Nanduni Kaveesha Senadeera <66235626+Senadeera-NK@users.noreply.github.com>
2025-09-25 06:10:54 +0000

    added the pie chart on dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -4,7 +4,30 @@ import Image from "next/image";
 import styles from "../page.module.css";
 import {useAuth} from "./../../components/AuthContext"
 import { Heading } from "@chakra-ui/react"
+import{Cell, Pie, PieChart, Legend, ResponsiveContainer} from "recharts";
+import {Box} from "@chakra-ui/react"
 
+const data = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const RADIAN = Math.PI / 180;
+
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: any) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-(midAngle ?? 0) * RADIAN);
+  const y = cy + radius * Math.sin(-(midAngle ?? 0) * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${((percent ?? 1) * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
 export default function Dashboard() {
       const {user} = useAuth();
@@ -12,6 +35,27 @@ export default function Dashboard() {
   return (
     <div className={styles.page}>
       <Heading position="absolute" top={5}>Welcome, Back! {userName}</Heading>
+      <Box width="100%" height="400px" marginTop="400px" display="flex" justifyContent="flex-start">
+        <ResponsiveContainer width={500} height="100%">
+          <PieChart>
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={120}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Legend layout="vertical" verticalAlign="middle" align="right"/>
+          </PieChart>
+        </ResponsiveContainer>
+      </Box>
     </div>
   );
 }
