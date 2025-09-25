8911167659b3a831276404bcdab4fd2c54e7bba4
S.A. Nanduni Kaveesha Senadeera <66235626+Senadeera-NK@users.noreply.github.com>
2025-09-25 07:04:28 +0000

    changes to the styles of the dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 import styles from "../page.module.css";
 import {useAuth} from "./../../components/AuthContext"
 import { Heading } from "@chakra-ui/react"
-import{Cell, Pie, PieChart, Legend, ResponsiveContainer} from "recharts";
+import{Cell, Pie, PieChart, BarChart, Bar, XAxis, YAxis, Legend, ResponsiveContainer, CartesianGrid, Tooltip} from "recharts";
 import {Box} from "@chakra-ui/react"
+
 
 const data = [
   { name: 'Group A', value: 400 },
@@ -33,18 +34,21 @@ export default function Dashboard() {
       const {user} = useAuth();
       const userName = user?.name || "Guest";
   return (
-    <div className={styles.page}>
+    <Box className={styles.page} padding="20px">
       <Heading position="absolute" top={5}>Welcome, Back! {userName}</Heading>
-      <Box width="100%" height="400px" marginTop="400px" display="flex" justifyContent="flex-start">
+
+      <Box display="flex" gap="50px" mb={10}>
+      {/* PIE CHART  => For the categorized files from the uploaded files */}
+      <Box width="400px" height="400px" marginTop="400px" display="flex" justifyContent="flex-start" paddingRight="100px">
         <ResponsiveContainer width={500} height="100%">
           <PieChart>
             <Pie
               data={data}
-              cx="50%"
+              cx="45%"
               cy="50%"
               labelLine={false}
               label={renderCustomizedLabel}
-              outerRadius={120}
+              outerRadius={150}
               fill="#8884d8"
               dataKey="value"
             >
@@ -52,10 +56,36 @@ export default function Dashboard() {
                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
               ))}
             </Pie>
-            <Legend layout="vertical" verticalAlign="middle" align="right"/>
+            <Legend layout="vertical" verticalAlign="middle" align="right" wrapperStyle={{marginLeft:'150px'}}/>
           </PieChart>
         </ResponsiveContainer>
       </Box>
-    </div>
+
+      {/* BAR CHART => For the usage of the site's sections of the user */}
+      <Box width="400px" height="400px" marginTop="400px">
+        <ResponsiveContainer width={600} height="100%">
+             <BarChart
+          data={data}
+          layout="vertical"  // horizontal bars
+          margin={{ top: 20, right: 30, left: 50, bottom: 20 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis type="number" />
+          <YAxis type="category" dataKey="name" />
+          <Tooltip />
+          <Bar dataKey="value">
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Bar>
+        </BarChart>
+        </ResponsiveContainer>    
+      </Box>
+      </Box>
+      {/* DAILY USAGE BAR CHART -> For the users's to show their hours of daily usage for 30 days */}
+      <Box width="100%" height="300px">  
+      
+      </Box>
+    </Box>
   );
 }
