60b4a8373499263a953fbc854e4105178b57cdf5
Senadeera NK <naduni.20191206@iit.ac.lk>
2025-10-14 11:17:38 +0530

    trying aouth

--- frontend/src/stack/client.tsx
+++ frontend/src/stack/client.tsx
@@ -8,7 +8,6 @@ urls: {
   home: "https://bookish-space-spoon-q69vx4j4qr42696p-3000.app.github.dev",
   afterSignIn: "https://bookish-space-spoon-q69vx4j4qr42696p-3000.app.github.dev/dashboard",
   afterSignUp: "https://bookish-space-spoon-q69vx4j4qr42696p-3000.app.github.dev/dashboard",
-  signIn: "https://bookish-space-spoon-q69vx4j4qr42696p-3000.app.github.dev/handler/signin",
 },
 
 });
--- frontend/src/stack/server.tsx
+++ frontend/src/stack/server.tsx
@@ -10,7 +10,6 @@ export const stackServerApp = new StackServerApp({
   home: "https://bookish-space-spoon-q69vx4j4qr42696p-3000.app.github.dev",
   afterSignIn: "https://bookish-space-spoon-q69vx4j4qr42696p-3000.app.github.dev/dashboard",
   afterSignUp: "https://bookish-space-spoon-q69vx4j4qr42696p-3000.app.github.dev/dashboard",
-  signIn: "https://bookish-space-spoon-q69vx4j4qr42696p-3000.app.github.dev/handler/signin",
 },
 
 });
