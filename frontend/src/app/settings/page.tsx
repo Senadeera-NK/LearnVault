a1102831b7246254c586e554b810550110bcb668
Senadeera NK <naduni.20191206@iit.ac.lk>
2025-10-25 00:03:06 +0530

    added some settings

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Box, Heading, Progress } from "@chakra-ui/react";
+import { Box, Heading, VStack, HStack, Text, Switch } from "@chakra-ui/react";
 import {usePageTimer} from "../../components/UsePageTimer";
 import {recordUsage} from "../../../api/api";
 import { useAuth } from "@/components/AuthContext";
 
-export default function Settings() { 
+export default function Settings() {
       const { user } = useAuth();
       // Track page usage
       usePageTimer("Settings", async (duration) => {
@@ -20,15 +20,48 @@ export default function Settings() {
             console.error("❌ Failed to record usage", err);
       }
       });
-
        console.log("Rendering Settings for user:", user);
+
+       const settingsOptions = [
+            {label:"Enable Notifications", key:"notifications"},
+            {label:"Dark Mode", key:"DarkMode"},
+            {label:"Auto-Save", key:"autoSave"},
+       ];
   return(
-     <Box className="styles.page"   
+      <>
+     <Box className="styles.page"
             display="flex"
             flexDirection="column"
             justifyContent="flex-start"
             pt="10px" >
-      <Heading mb="20px" textAlign="center" w="100%">
-Settings</Heading>
+      <Heading mb="20px" textAlign="center" w="100%">Settings</Heading>
       </Box>
+      <Box
+            w="70%"
+            mx="auto"
+            border="1px solid"
+            borderColor="gray.300"
+            borderRadius="lg"
+            mt={8}
+            mb={8}
+            p={3}
+            >
+            <VStack spacing={4} align="stretch">
+            {settingsOptions.map((option) => (
+                  <HStack
+                  key={option.key}
+                  justifyContent="space-between"
+                  p={3}
+                  w="100%"
+                  border="1px solid"
+                  borderColor="gray.200"
+                  borderRadius="md"
+                  >
+                  <Text>{option.label}</Text>
+                  <Switch />
+                  </HStack>
+            ))}
+            </VStack>
+            </Box>
+      </>
   );}
