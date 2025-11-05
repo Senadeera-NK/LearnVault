1af39b26790ea011a8088d52fa5a0bf7f652fa94
Senadeera NK <naduni.20191206@iit.ac.lk>
2025-11-05 11:54:38 +0530

    updated the trufalseskeleton to store the answers of user

--- frontend/src/app/qa/components/TrueFalseSkeleton.tsx
+++ frontend/src/app/qa/components/TrueFalseSkeleton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import {   Box,Heading,Button,VStack,Text,IconButton,Portal, } from "@chakra-ui/react";
+import {Box,Button,VStack,Text,HStack, Stack} from "@chakra-ui/react";
+import { useState } from "react";
 
 interface TrueFalseItem {
     question:string;
@@ -10,14 +11,21 @@ interface TrueFalseItem {
 interface TrueFalseSkeletonProps{
     data:TrueFalseItem[];
 }
-export default function MCQskeleton({ data }:TrueFalseSkeletonProps){
+export default function TrueFalseSkeleton({ data }:TrueFalseSkeletonProps){
+    const [selectedAnswer, setSelectedAnswer] = useState<Record<number, string>>({});
+    const handleSelect = (index:number, value:string) =>{
+        setSelectedAnswer((prev)=>({...prev,[index]:value,}));
+    };
+
     return(
         <VStack align="stretch" spacing={3}>
             {data.map((qa,index)=>(
                 <Box key={index} p={3} border="1px solid" borderColor="gray.200">
                     <Text fontWeight="semibold" pb={2}>{index+1}. {qa.question}</Text>
-                    <Button value="true">True</Button>
-                    <Button value="false">False</Button>
+                    <HStack align="stretch" spacing={2}>
+                        <Button value="true" colorScheme={selectedAnswer[index]==="true"?"green":"gray"} onClick={()=>handleSelect(index, "true")}>True</Button>
+                        <Button value="true" colorScheme={selectedAnswer[index]==="false"?"red":"gray"} onClick={()=>{handleSelect(index, "false")}}>False</Button>
+                    </HStack>
                 </Box>
             ))}
         </VStack>
