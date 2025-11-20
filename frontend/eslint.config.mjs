f0a1b790e42a232078c954c160572d81793176a9
Senadeera NK <naduni.20191206@iit.ac.lk>
2025-11-20 18:01:52 +0530

    changes

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -20,6 +20,14 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/exhaustive-deps": "warn",   // optional
+      "@typescript-eslint/no-unused-vars": "warn"
+    }
+  }
 ];
+
 
 export default eslintConfig;
