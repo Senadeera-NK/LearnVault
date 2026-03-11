ca1234bd58814b1f0dec473c4ac44a5be0990ac5
Senadeera NK <naduni.20191206@iit.ac.lk>
2026-03-11 19:03:10 +0530

    chore: frontend react package update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,9 @@
 import Providers from "./providers";
 import ClientLayout from "./client-layout";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <Providers>
           <ClientLayout>{children}</ClientLayout>
